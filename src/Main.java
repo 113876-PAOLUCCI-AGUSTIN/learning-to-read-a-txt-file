700d5631bddc7eab8dc3039b763116098f0d85cd
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-04-10 17:00:43 -0300

    Get element at X index.

--- src/Main.java
+++ src/Main.java
@@ -33,5 +33,12 @@ public class Main {
         }
 
         System.out.println("The actual ArrayList size is: " +top1000BabyGirlsNamesUSAList.size()+".");
+        System.out.println(top1000BabyGirlsNamesUSAList.lastIndexOf("Anne"));
+        System.out.println("1st " + top1000BabyGirlsNamesUSAList.get(0));
+        System.out.println("250th " + top1000BabyGirlsNamesUSAList.get(250));
+        System.out.println("500th " + top1000BabyGirlsNamesUSAList.get(500));
+        System.out.println("750th " + top1000BabyGirlsNamesUSAList.get(750));
+
+
     }
 }
