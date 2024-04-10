0dbaf084bc5862c5686dc73acc2d0b1c0407c0b1
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-04-10 15:55:22 -0300

    Read and create ArrayList

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,17 @@ public class Main {
     public static void main(String[] args) {
         File file = new File(
                 "C:\\top1000BabyGirlsNamesUSA.txt");
+
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        ArrayList<String> top1000BabyGirlsNamesUSAList = new ArrayList<>();
+        while (sc.hasNextLine())
+            top1000BabyGirlsNamesUSAList.add(sc.nextLine());
+        sc.close();
     }
 }
